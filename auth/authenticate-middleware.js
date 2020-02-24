20d594ea94d724dcc0fca8bb410bcde94e8acc5d
Jimmy Hooker <jimmyjjwalker@gmail.com>
2020-02-23 20:21:25 -0800

    Everything except for tests

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,20 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets.js");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  if (req.decodedJwt) {
+    next();
+  } else if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedJwt) => {
+      if (err) {
+        res.status(401).json({ you: "No sir, no how" });
+      } else {
+        req.decodedJwt = decodedJwt;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ you: "Not allowed son" });
+  }
 };
